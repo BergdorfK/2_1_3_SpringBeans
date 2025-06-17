d809c65e396014af8f37f0fbadb80f96a49ecb55
BergdorfK <bergdorf.kolos@gmail.com>
2025-06-17 12:44:14 +0300

    fix: task 2_1_3 add getTimer

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -14,6 +14,11 @@ public class AnimalsCage {
     @Autowired
     private Timer timer;
 
+    public Timer getTimer() {
+        return timer;
+    }
+
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
